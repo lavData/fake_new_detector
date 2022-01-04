59b6ba2500b4564d98dd5a129ea7172fa6a49489
vucan1892000@gmail.com <vucan1892000@gmail.com>
2022-01-04 19:23:05 +0700

    Chốt python

--- pr.py
+++ pr.py
@@ -1,18 +1,18 @@
 import string
-import numpy as np
 import pandas as pd
 from PIL import Image
 import streamlit as st
 from pyvi import ViTokenizer
 from sklearn.metrics import accuracy_score
 from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegressionCV
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+def remove_stopword(x):
+     return [i.rstrip('_') for i in x if i not in stopword and i not in [',', '']]
 
 image = Image.open('./image.jpeg')
 st.image(image)
@@ -21,28 +21,29 @@ st.title('ENGLISH FAKE NEW DETECTOR')
 model =  st.selectbox('Model you want',['LogisticRegressionCV',\
                                         'KNeighborsClassifier',\
                                         'MLPClassifier'])
+
 text =  st.text_area('Piece of news here')
+
 news_df = pd.read_csv('./vn_news_223_tdlfr.csv', encoding='utf-8')
 stopword = open('./vietnamese-stopwords.txt', 'r',encoding='utf-8')
 stopword = set([i.rstrip() for i in stopword.readlines()])
 
-def preprocessing(text):
-    chars_to_remove = list(string.punctuation)
-    chars_to_remove.extend(['“', '”'])
-    
-    no_punc = ''.join([char for char in text if char not in chars_to_remove])
-    no_punc = no_punc.replace('\n', ' ')
-    
-    tokenize = ViTokenizer.tokenize(no_punc)
-    
-    clean_words = [word.lower() for word in tokenize.split() if word.lower() not in stopword]
-    return clean_words
-
-vectorizer = TfidfVectorizer(analyzer=preprocessing)
 
 new_df = pd.DataFrame({'text': [text], 'domain': [''], 'label': ['']})
 new_df = pd.concat([news_df, new_df], axis=0, ignore_index=True)
-train_data = vectorizer.fit_transform(new_df['text'])
+
+puncs = string.punctuation + '\n“”‘’'
+stopword = pd.Series(list(stopword)).str.replace(' ', '_').to_list()
+
+
+clean_text = new_df['text'].replace(f'[{puncs}]', ',', regex=True).\
+             apply(ViTokenizer.tokenize).str.lower().\
+             str.split().apply(remove_stopword)
+
+new_df['clean_text'] = clean_text
+new_df['clean_text'] = new_df.apply(lambda x: ' '.join(x['clean_text']) + ' ' + x['domain'], axis=1)
+train_data = TfidfVectorizer(lowercase=False).fit_transform(new_df['clean_text'])
+
 x_train = train_data[:-1]
 x_test = train_data[-1:]
 
@@ -51,22 +52,26 @@ if st.button('Predict it news is real or fake 👈'):
 
 
     if model == 'LogisticRegressionCV':
-        lg_re = LogisticRegressionCV(Cs=20, cv=5, solver='lbfgs', max_iter=10000).fit(x_train, news_df.label)
+        lg_re = LogisticRegressionCV(Cs=20, cv=5, solver='newton-cg', max_iter=10000).\
+                fit(x_train, news_df.label)
         result = lg_re.predict(x_test)
+        st.write(result)
         if result[0] == 0:
             st.write('fake new')
         else:
             st.write('real new')
         
-    elif model == 'KNeighborsClassifier':
-        k_neighbor = KNeighborsClassifier(n_neighbors=6, algorithm='auto').fit(x_train, news_df.label)
-        result = k_neighbor.predict(x_test)
+    elif model == 'DecisionTreeClassifier':
+        tree = DecisionTreeClassifier(random_state=42, max_features=None, max_leaf_nodes=30).\
+                     fit(x_train, news_df.label)
+        result = tree.predict(x_test)
         if result[0] == 0:
             st.write('fake new')
         else:
             st.write('real new')
     else:
-        MLPC = MLPClassifier(hidden_layer_sizes=(50), alpha=0, solver='lbfgs', max_iter=10000).fit(x_train, news_df.label)
+        MLPC = MLPClassifier(hidden_layer_sizes=(50), alpha=0, solver='lbfgs', max_iter=10000).\
+               fit(x_train, news_df.label)
         result = MLPC.predict(x_test)
         if result[0] == 0:
             st.write('fake new')
